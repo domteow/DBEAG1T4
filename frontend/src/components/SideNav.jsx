3fa5e899c4abd59041f38fda46b5a9c89618f777
Alvin <alvinngow21@gmail.com>
2023-03-23 11:34:10 +0800

    Configure background color Configured SideNav links

--- frontend/src/components/SideNav.jsx
+++ frontend/src/components/SideNav.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/links.css';
 
 function SideNav() {
-	return <div className=' bg-card h-screen p-4'></div>;
+	return (
+		<div className=' bg-card h-screen p-4'>
+			<Link to='/home' className='link'>
+				Home
+			</Link>
+		</div>
+	);
 }
 
 export default SideNav;
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				'bg-primary': '#141332',
+				background: '#141332',
 				primary: '#6359e9',
 				secondary: '#64cff6',
 				card: '#1d1d41',
