6159f87873ae2b277a391927815eb9c0c51eaa4a
Alvin <alvinngow21@gmail.com>
2023-03-23 13:05:44 +0800

    Login page frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import '../styles/login.css';
+import logo from '/logo-no-background.png';
 
 function Login() {
-	return <div>Login</div>;
+	return (
+		<div className='login'>
+			<div className='login-box '>
+				<img src={logo} className='h-[50px]' alt='' />
+
+				<input
+					className='rounded clearable w-full p-2 '
+					placeholder='Username'
+				/>
+				<input
+					type='password'
+					className='rounded clearable w-full p-2'
+					placeholder='Password'
+				/>
+				<button
+					className='login-button'
+					onClick={() => {
+						setTimeout(() => navigate('/home'), 1000);
+					}}
+				>
+					Login
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
