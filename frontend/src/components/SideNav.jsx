70a5cf5ab4b3bcd15a7c6ed73d0278c63f0fa281
Alvin <alvinngow21@gmail.com>
2023-03-23 17:05:26 +0800

    Working logout

--- frontend/src/components/SideNav.jsx
+++ frontend/src/components/SideNav.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/links.css';
-import { HomeIcon } from '@heroicons/react/24/outline';
+import '../styles/nav.css';
+import { HomeIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import NavItem from './common/NavItem';
 import { useNavigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../auth';
 
 function SideNav() {
 	const navigate = useNavigate();
 	const location = useLocation();
+	const [arrow, setArrow] = useState(false);
+
+	const auth = useAuth();
+	const user = auth.user;
+	console.log(user);
 	return (
 		<div className='nav'>
-			<NavItem
-				label='Home'
-				Icon={() => <HomeIcon className='h-6 w-6' />}
-				name='nav'
-				id='home'
-				onClick={() => navigate('home')}
-				checked={location.pathname === '/home'}
-			/>
+			<div className='nav-items'>
+				<NavItem
+					label='Home'
+					Icon={() => <HomeIcon className='h-6 w-6' />}
+					name='nav'
+					id='home'
+					onClick={() => navigate('home')}
+					checked={location.pathname === '/home'}
+				/>
+			</div>
+			<div className='profile-panel'>
+				<UserCircleIcon className='h-10 w-10 text-white' />
+				<div>
+					<p className='font-bold'>
+						{user.givenName} {user.familyName}
+					</p>
+				</div>
+				<button className='subtitle' onClick={() => auth.logout()}>
+					Logout
+				</button>
+			</div>
 		</div>
 	);
 }
--- frontend/src/components/common/RequireAuth.jsx
+++ frontend/src/components/common/RequireAuth.jsx
@@ -5,7 +5,7 @@ import { useAuth } from '../../auth';
 function RequireAuth({ children }) {
 	const auth = useAuth();
 
-	const user = localStorage.getItem('user');
+	const user = JSON.parse(localStorage.getItem('user'));
 	if (user) {
 		auth.user = user;
 	}
