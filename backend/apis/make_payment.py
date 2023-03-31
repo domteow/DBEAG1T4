17ef4f43616238cdfb7bc95c03ebd0351ef0a3e6
domteow <dominicteow@gmail.com>
2023-04-01 03:18:04 +0800

    left with noti and logging

--- backend/apis/make_payment.py
+++ backend/apis/make_payment.py
@@ -6,6 +6,7 @@ from os import environ
 
 import requests
 import json
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
@@ -20,90 +21,168 @@ notification_URL = environ.get('notification_URL') or "http://localhost:5002/not
 def make_payment():
     """
     Make payment
-    -> take in lender_id, loan_request_id, borrower_account_id
+    -> take in payer_id, loan_request_id, payee_account_id
     -> get lender account num by lender_id
     -> update row in loan_request (add in lender_id, monthly_installment, amount_left, update "status)
     -> use payment to transfer money
     -> use transaction to keep record
     """
-    # 1. Get lender_id, loan_request_id, borrower_account_id
+
+    def calculate_monthly_installment(loan_amount, annual_interest_rate, maturity_date_str):
+        maturity_date = datetime.strptime(maturity_date_str, '%Y-%m-%d')
+
+        monthly_interest_rate = annual_interest_rate / 12
+
+        n = (maturity_date.year - datetime.now().year) * 12 + (maturity_date.month - datetime.now().month)
+        
+        monthly_installment = loan_amount * (monthly_interest_rate * (1 + monthly_interest_rate) ** n) / ((1 + monthly_interest_rate) ** n - 1)
+        
+        return monthly_installment
+
+
+
+    # 1. Get payer_id, loan_request_id, payee_id, payment amt
     data = request.get_json()
-    lender_id = data['Header']['userID']
+    payer_id = data['Header']['userID']
     PIN = data['Header']['PIN']
     OTP = data['Header']['OTP']
-    borrower_account_id = data['Content']['accountID']
+    payee_account_id = data['Content']['accountID']
     loan_request_id = data['Content']['loan_request_id']
+    payment_with_commission = data['Content']['payment_amount']
+    commission = payment_with_commission * 0.01
+    payment_amount = payment_with_commission - commission
 
-    # # 2. Get borrower_id using loan request id
-    # loan_request = requests.get(loan_request_URL + loan_request_id)
-    # borrower_id = loan_request.json()['data']['borrower_id']
+    
 
-    # 3. Get lender account id using lender id
-    lender_headerObj = {
+    # 2. Get payer account id using payer id
+    payer_headerObj = {
         "Header" : {
             "serviceName" : "getCustomerAccounts",
-            "userID" : lender_id,
+            "userID" : payer_id,
             "PIN" : PIN,
             "OTP" : OTP
         }
     }
 
-    lender_response = requests.post(customer_URL, json=lender_headerObj)
-    if lender_response.json()['code'] != 200:
+    payer_response = requests.post(customer_URL, json=payer_headerObj)
+    if 300 > payer_response.json()['code'] > 200:
         return jsonify(
             {
                 "code": 500,
                 "data": {},
-                'message': 'Payment Failed, failed to get lender account details'
+                'message': 'Failed to get payer account details'
             } 
         )
-    lender_account_details = lender_response.json()['data']['Content']['ServiceResponse']['AccountList']
-    lender_account_id
+    payer_account_details = payer_response.json()['data']['Content']['ServiceResponse']['AccountList']
+    payer_account_id = payer_account_details["account"]["accountID"]
+    payer_account_id = payer_account_id.lstrip("0")
+
+    # 3. get data from Loan Request DB
+    loan_request_response = requests.get(loan_request_URL + loan_request_id)
+
+    if 300 > loan_request_data.json()['code'] > 200:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {},
+                'message': 'Failed to get loan request details'
+            } 
+        )
+    
+    loan_request_data = loan_request_response.json()['data']
+
+    # 4. Check if loan request is still open and valid
+    if loan_request_data['amount_left'] < payment_amount:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {},
+                'message': 'Payment amount is more than amount left'
+            } 
+        )
+    
+    elif loan_request_data['amount_left'] <= 0 :
+        return jsonify(
+            {
+                "code": 500,
+                "data": {},
+                'message': 'Amount is 0 or negative or null'
+            } 
+        )
+    
+    elif loan_request_data['status'] == "closed":
+        return jsonify(
+            {
+                "code": 500,
+                "data": {},
+                'message': 'Loan request is closed'
+            } 
+        )
+    
+    
+
+    # 5. Make payment
+    payment_requestObj = {
+                "Header" : {
+                    "serviceName" : "creditTransfer",
+                    "userID" : payer_id,
+                    "PIN" : PIN,
+                    "OTP" : OTP
+                },
+
+                "Content" : {
+                    "accountFrom" : payer_account_id,
+                    "accountTo" : payee_account_id,
+                    "transactionAmount" : payment_amount
+                }
+            }
+    
+    payment_response = requests.post(payment_URL, json=payment_requestObj)
+    if payment_response.json()['code'] > 300:
+        return jsonify(
+            {
+                "code": 500,
+                "data": payment_response.json(),
+                'message': 'Failed to make payment'
+            } 
+        )
+    
+    # 6. Edit Loan Request in DB
+
+    # Lender to Borrower (sends full principal amount)
+    if loan_request_data['status'] == "request":
+        loan_request_data['lender_id'] = payer_id
+        monthly_installment = calculate_monthly_installment(payment_amount, loan_request_data['interest_rate'], loan_request_data['maturity_date'])
+        loan_request_data['monthly_installment'] = monthly_installment
+        loan_request_data['amount_left'] = loan_request_data['loan_amount']
+        loan_request_data['status'] = "active"
+        update_loan_request = requests.put(loan_request_URL + loan_request_id, json=loan_request_data)
+
+
+    elif loan_request_data['status'] == "active":
+        loan_request_data['amount_left'] -= payment_amount
+
+        if loan_request_data['amount_left'] <= 0:
+            loan_request_data['status'] = "closed"
+        update_loan_request = requests.put(loan_request_URL + loan_request_id, json=loan_request_data)
+
+
+    # 7. Send notification to payer
+
+
+    # 8. Log transaction
+
+
 
 
     
 
+        
+    
 
 
-    # 1. Call payment service to make payment
-    response = requests.post(payment_URL, json=data)
-    print("Payment service response:", response.json())
 
-    # 2. Call transaction service to create transaction record
-    if response.status_code == 200:
-        data = response.json()['data']
-        data['paymentID'] = data['Content']['ServiceResponse']['ServiceRespBody']['PaymentID']
-        data['paymentStatus'] = data['Content']['ServiceResponse']['ServiceRespBody']['PaymentStatus']
-        # del data['Content']
-        print("Data to be sent to transaction service:", data)
-
-        response = requests.post(transaction_URL, json=data)
-        print("Transaction service response:", response.json())
-
-        if response.status_code == 200:
-            return jsonify(
-                {
-                    "code": 200,
-                    "data": response.json(),
-                    'message': 'Payment Successful'
-                } 
-            )
-        else:
-            return jsonify(
-                {
-                    "code": 500,
-                    "data": {},
-                    'message': 'Payment Failed'
-                } 
-            )
-    else:
-        return jsonify(
-            {
-                "code": 500,
-                "data": {},
-                'message': 'Payment Failed'
-            } 
-        )
+    
 
 
 
